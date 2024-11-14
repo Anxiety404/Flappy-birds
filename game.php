ec35fe00da8a0cf35de1c738bb2736805c74034f
jay van_der_kooye <j.vanderkooye@student.talland.nl>
2024-11-14 19:07:05 +0100

    Starting flappy birds

---
+++ game.php
@@ -0,0 +1,10 @@
+/*
+Making flappy birds in php.
+
+Make a object that moves up with spacebar and up arrow and goes down with the given grafity.
+Make a loop of objects that spawn at a random number 
+
+
+
+
+*/
